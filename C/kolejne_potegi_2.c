f6a364ff4d4db93a77fe32417c8ef239d78903a1
Daniel Jakubek <k.a.s.z.u.b@wp.pl>
2013-11-24 10:21:29 +0100

    Program potega 2 dwojki

---
+++ C/kolejne_potegi_2.c
@@ -0,0 +1,17 @@
+/*Program, który wypisuje kolejne potęgi 
+liczby 2 nie przekraczające 2010.*/
+#include <stdio.h>
+
+int main()
+{
+    int wynik = 1;
+   /* while (wynik <= 2010) {  // innna mozliwosc gorsza
+	printf("%d \n", wynik);    
+	wynik *= 2;
+    } */
+    for (wynik = 1; wynik <= 2010; wynik *= 2)
+    {
+    	printf("%d \n", wynik);
+    }
+    return 0;
+}
